572f363bf932d2609cf1c0052dcb0eaa9e9e37df
dilpreetsio <singhsince94@gmail.com>
2022-06-01 22:00:02 +0530

    update readme

--- pages/index.js
+++ pages/index.js
@@ -30,11 +30,11 @@ export default function Home() {
         <input onChange={(e) => setTweet(e.currentTarget.value)}
         aria-describedby="helper-text-explanation"
         className="mt-40 mb-20 w-fit bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block p-2.5" placeholder="Enter tweet id"/>
-        <div ref={tweetCanvas} className="w-fit flex flex-col justify-content items-center bg-gradient-to-r from-cyan-500 to-green-500">
+        <div ref={tweetCanvas} className="w-fit flex flex-col justify-content items-center bg-gradient-to-r from-cyan-500 to-indigo-500">
           <Tweet id={tweet || DEFAULT} className="p-20"/>
         </div>
         <button type="button" onClick={exportImage}
-        className="mt-4 text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-4 focus:ring-green-400
+        className="mt-20 text-white bg-black hover:bg-green-700 focus:outline-none focus:ring-4 focus:ring-green-400
         font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 ">Export Image</button>
       </div>
     </div>
